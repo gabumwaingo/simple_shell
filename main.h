9c14424d28dc73af426f59a2ca57fecdcc700d42
gabumwaingo <gabumwaingo@gmail.com>
2023-02-22 00:25:19 -0800

    Debugging

---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *read_line(void);
+char **split_line(char *buff);
+int bind(char **arr);
+
+#endif
--- shell.c
+++ shell.c
@@ -5,50 +5,132 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
-int main(int argc, char **argv)
+int _cd(char **arr);
+int _alias(char **arr);
+int ls_exit(char **arr);
+int _env(char **arr);
+/**
+ * read_line - reads input from the user
+ * Return: pointer to the buffer storing the line
+ * that was read
+ */
+char *read_line(void)
 {
 	char *buff = NULL;
 	size_t n = 0;
-	char *stkn;
-	pid_t my_pid;
-	int status, i;
 
-	while (1)
+	if (getline(&buff, &n, stdin) == -1)
 	{
-		printf("$ ");
-		getline(&buff, &n, stdin);
-
-		stkn = strtok(buff, " \n");
-
-		char **arr = malloc(sizeof(char *) * 32);
-		arr[0] = stkn;
-
-		if (strcmp(arr[0], "exit") == 0)
+		if (feof(stdin))
+		{
 			exit(0);
-
-		i = 1;
-		while (stkn != NULL)
-		{
-			stkn = strtok(NULL, " \n");
-			arr[i] = stkn;
-			++i;
-		}
-
-		my_pid = fork();
-		if (my_pid == -1)
-		{
-			perror("Error");
-			return (1);
-		}
-		else if (my_pid == 0)
-		{
-			execve(arr[0], arr, NULL);
 		}
 		else
 		{
-			wait(&status);
+			perror("Error");
+			exit(1);
 		}
 	}
-	free(buff);
+	return (buff);
+}
+
+/**
+ * split_line - splits the inputed line
+ * @buff: the line to be split
+ * Return: array of words thatwere split
+ */
+char **split_line(char *buff)
+{
+	char **arr;
+	char *stkn;
+	int i;
+
+	arr = malloc(sizeof(char) * 32);
+	stkn = strtok(buff, " \n");
+	arr[0] = stkn;
+
+	i = 1;
+	while (stkn != NULL)
+	{
+		stkn = strtok(NULL, " \n");
+		arr[i] = stkn;
+		++i;
+	}
+	arr[i] = NULL;
+	return (arr);
+}
+/**
+ * execute - executes the commands inputted
+ * by the users
+ * @arr: source pof the commands and arguments
+ * Return: 1 on success
+ */
+int execute(char **arr)
+{
+	pid_t my_pid;
+	int status;
+
+	if (my_pid == 0)
+	{
+		if (execve(arr[0], arr, NULL) == -1)
+		{
+			perror("Error");
+		}
+		exit(EXIT_FAILURE);
+	}
+	else if (my_pid < 0)
+	{
+		perror("Error");
+	}
+	else
+	{
+		wait(&status);
+	}
+	return (1);
+}
+
+char *builtin_list[] = {
+	"exit"
+};
+int (*builtin_fun[])(char **) = {
+	&ls_exit
+};
+
+/**
+ * num_builtins - counts the number of builtins
+ * Return: number of builtins
+ */
+int num_builtins(void)
+{
+	return (sizeof(builtin_list) / sizeof(char *));
+}
+/**
+ * ls_exit -  exits the shell
+ * @arr: user input
+ * Return: 0
+ */
+int ls_exit(char **arr)
+{
 	return (0);
+}
+/**
+ * bind - incoporates all functions
+ * @arr:  user input
+ * Return: executed commands
+ */
+int bind(char **arr)
+{
+	int i;
+
+	if (arr[0] == NULL)
+	{
+		return (1);
+	}
+
+	for (i = 0; i < num_builtins(); i++)
+	{
+		if (strcmp(arr[0], builtin_list[i]) == 0)
+			return (builtin_fun[i](arr));
+	}
+	return (execute(arr));
 }
