26360d3406a735cc7734c179fcbbdf1b38247c77
iakah <iakah2313@gmail.com>
2023-02-22 06:15:03 -0800

    temporary

--- shell.c
+++ shell.c
@@ -1,44 +1,90 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include "main.h"
-
-char *builtin_list[] = {
-	"exit"
-};
-int (*builtin_fun[])(char **) = {
-	&ls_exit
-};
+#include "shell.h"
 
 /**
- * num_builtins - counts the number of builtins
- * Return: number of builtins
+ * main - Simple Shell (Hsh)
+ * @argc: Argument Count
+ * @argv:Argument Value
+ * Return: Exit Value By Status
  */
-int num_builtins(void)
+
+int main(__attribute__((unused)) int argc, char **argv)
 {
-	return (sizeof(builtin_list) / sizeof(char *));
+	char *input, **cmd;
+	int counter = 0, statue = 1, st = 0;
+
+	if (argv[1] != NULL)
+		read_file(argv[1], argv);
+	signal(SIGINT, signal_to_handel);
+	while (statue)
+	{
+		counter++;
+		if (isatty(STDIN_FILENO))
+			prompt();
+		input = _getline();
+		if (input[0] == '\0')
+		{
+			continue;
+		}
+		history(input);
+		cmd = parse_cmd(input);
+		if (_strcmp(cmd[0], "exit") == 0)
+		{
+			exit_bul(cmd, input, argv, counter);
+		}
+		else if (check_builtin(cmd) == 0)
+		{
+			st = handle_builtin(cmd, st);
+			free_all(cmd, input);
+			continue;
+		}
+		else
+		{
+			st = check_cmd(cmd, input, counter, argv);
+
+		}
+		free_all(cmd, input);
+	}
+	return (statue);
 }
 /**
- * bind - incoporates all functions
- * @arr:  user input
- * Return: executed commands
+ * check_builtin - check builtin
+ *
+ * @cmd:command to check
+ * Return: 0 Succes -1 Fail
  */
-int bind(char **arr)
+int check_builtin(char **cmd)
+{
+	bul_t fun[] = {
+		{"cd", NULL},
+		{"help", NULL},
+		{"echo", NULL},
+		{"history", NULL},
+		{NULL, NULL}
+	};
+	int i = 0;
+		if (*cmd == NULL)
+	{
+		return (-1);
+	}
+
+	while ((fun + i)->command)
+	{
+		if (_strcmp(cmd[0], (fun + i)->command) == 0)
+			return (0);
+		i++;
+	}
+	return (-1);
+}
+/**
+ * creat_envi - Creat Array of Enviroment Variable
+ * @envi: Array of Enviroment Variable
+ * Return: Void
+ */
+void creat_envi(char **envi)
 {
 	int i;
 
-	if (arr[0] == NULL)
-	{
-		return (1);
-	}
-
-	for (i = 0; i < num_builtins(); i++)
-	{
-		if (strcmp(arr[0], builtin_list[i]) == 0)
-			return (builtin_fun[i](arr));
-	}
-	return (execute(arr));
+	for (i = 0; environ[i]; i++)
+		envi[i] = _strdup(environ[i]);
+	envi[i] = NULL;
 }
